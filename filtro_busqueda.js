3f1550e575051b7f0f8e04a634f90932ba0196e9
Carlos Medina <keystrike95@gmail.com>
2023-05-26 00:29:21 -0600

    Using the keyup addEventListener

--- filtro_busqueda.js
+++ filtro_busqueda.js
@@ -4,21 +4,7 @@ export default function search_filter(categoryNames, searchbarSection10) {
     searchbarContent,
     regExp;
 
-  document.addEventListener("input", (e) => {
-    if (e.target === searchbar) {
-      searchbarContent = searchbar.value;
-      regExp = new RegExp(`${searchbarContent}`, "gi");
-
-      categories.forEach((el) => {
-        if (!regExp.test(el.textContent))
-          el.parentElement.style.setProperty("display", "none");
-        else el.parentElement.style.setProperty("display", "block");
-      });
-    }
-  });
-
-  /* addEventListener("keyup", (e) => {
-    console.log("FIGCAPTION: ", categories);
+  addEventListener("keyup", (e) => {
     searchbarContent = searchbar.value;
     regExp = new RegExp(`${searchbarContent}`, "gi");
 
@@ -29,5 +15,5 @@ export default function search_filter(categoryNames, searchbarSection10) {
         el.parentElement.style.setProperty("display", "none");
       else el.parentElement.style.setProperty("display", "block");
     });
-  }); */
+  });
 }
