8386c65cdcaf8f09ee8c0fe4595a8a33d8a5d628
Carlos Medina <keystrike95@gmail.com>
2023-05-26 00:31:04 -0600

    using the input addEventListener

--- filtro_busqueda.js
+++ filtro_busqueda.js
@@ -4,16 +4,16 @@ export default function search_filter(categoryNames, searchbarSection10) {
     searchbarContent,
     regExp;
 
-  addEventListener("keyup", (e) => {
-    searchbarContent = searchbar.value;
-    regExp = new RegExp(`${searchbarContent}`, "gi");
+  document.addEventListener("input", (e) => {
+    if (e.target === searchbar) {
+      searchbarContent = searchbar.value;
+      regExp = new RegExp(`${searchbarContent}`, "gi");
 
-    if (e === "Escape") searchbar.textContent = ""; //Sí tecla Esc, entonces se borra lo escrito
-
-    categories.forEach((el) => {
-      if (!regExp.test(el.textContent))
-        el.parentElement.style.setProperty("display", "none");
-      else el.parentElement.style.setProperty("display", "block");
-    });
+      categories.forEach((el) => {
+        if (!regExp.test(el.textContent))
+          el.parentElement.style.setProperty("display", "none");
+        else el.parentElement.style.setProperty("display", "block");
+      });
+    }
   });
 }
